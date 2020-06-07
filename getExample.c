058af600f417c252e2255999369313ed96b89930
igor <igoreira0@gmail.com>
2020-06-07 20:20:53 -0300

    add search a specific id in command line example ./yourEXE 23 or all data if not param specificated

--- getExample.c
+++ getExample.c
@@ -5,34 +5,64 @@
 #define PATH_SAVE_REQUEST "--output /tmp/read.txt"
 #define SERVER_TO_REQUEST "jsonplaceholder.typicode.com/posts"
 #define GET_HEADER "curl -I --head"
+#define GET_BY_ID "?id="
 
-void main(){
-
-    FILE *fh = fopen("/tmp/read.txt","rb");
+void main(int argc, char *argv[ ]){
+    int i;
+    FILE *fh;
     size_t len;
     char *buf,*path;
-    path = malloc( (sizeof(SERVER_TO_REQUEST)) + (sizeof(PATH_SAVE_REQUEST)) + (sizeof(GET_HEADER)) );
-    sprintf(path,"%s %s %s",GET_HEADER,SERVER_TO_REQUEST,PATH_SAVE_REQUEST);
-    printf("%s\n",path);
-    system(path);
+    if(argc == 1){
+        path = malloc( (sizeof("curl ")) + (sizeof(SERVER_TO_REQUEST)) + (sizeof(PATH_SAVE_REQUEST)) );
+        sprintf(path,"curl %s %s",SERVER_TO_REQUEST,PATH_SAVE_REQUEST);
+        printf("%s\n",path);
+        system(path);
+        fh = fopen("/tmp/read.txt","rb");
+        if(!fh){
+            printf("cant open file in %s",PATH_SAVE_REQUEST);
+            return;
+        }
 
-    if(!fh){
-        printf("cant open file in %s",PATH_SAVE_REQUEST);
-        return;
+        fseek(fh, 0, SEEK_END);
+        len = ftell(fh);
+        rewind(fh);
+
+        buf = malloc(len);
+        fread(buf, 1, len, fh);
+        buf[len] = '\0';
+
+        printf("\nJSON RECEIVED:\n%s",buf);
+        free(buf);
+        free(path);
+        pclose(fh);
     }
+    for(i=1;i<argc;i++)
+    {
+        if(argc>1)   
+        {
+            path = malloc( (sizeof("curl ")) + (sizeof(SERVER_TO_REQUEST)) + (sizeof(PATH_SAVE_REQUEST)) + (sizeof(GET_BY_ID)) + (strlen(argv[i])) );
+            sprintf(path,"curl %s%s%s %s",SERVER_TO_REQUEST,GET_BY_ID,argv[i],PATH_SAVE_REQUEST);
+            printf("%s\n",path);
+        }
+        else return;
+        system(path);
+        fh = fopen("/tmp/read.txt","rb");
+        if(!fh){
+            printf("cant open file in %s",PATH_SAVE_REQUEST);
+            return;
+        }
 
-    fseek(fh, 0, SEEK_END);
-    len = ftell(fh);
-    rewind(fh);
+        fseek(fh, 0, SEEK_END);
+        len = ftell(fh);
+        rewind(fh);
 
-    buf = malloc(len);
-    fread(buf, 1, len, fh);
+        buf = malloc(len);
+        fread(buf, 1, len, fh);
+        buf[len] = '\0';
 
-    if(memcmp(buf, "HTTP/1.1 200",12) == 0){
-        printf("200 OK ! \n\n");
-    }else printf("can't connect to %s\n",SERVER_TO_REQUEST);
-    free(buf);
-    free(path);
-    pclose(fh);
-
+        printf("\nJSON RECEIVED:\n%s",buf);
+        free(buf);
+        free(path);
+        pclose(fh);
+    }
 }
